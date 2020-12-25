5a1a1e4ce3f9afc63ba0a5a30ebc9928dff2d466
trrrrr2312 <72880563+trrrrr2312@users.noreply.github.com>
2020-12-25 14:17:03 +0800

    11.25

--- canvas/pages/index/index.vue
+++ canvas/pages/index/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="box" style="height: 500px;">
-		<canvas canvas-id="canvass" id="canvass" @error="canvasIdErrorCallback()">
+		<canvas canvas-id="canvass" id="canvass" @touchstart="touchStart" @touchmove="touchMove" @touchend="touchEnd">
 		</canvas>
 
 	</view>
@@ -17,13 +17,14 @@
 				ctx: null,
 			}
 		},
-		mounted() {				
+		mounted() {
 			this.drawCanvas();
+			this.canvasEventsInit();
 
 		},
 		methods: {
 
-			drawCanvas(x,y) {
+			drawCanvas(x, y) {
 				var ctx = uni.createCanvasContext('canvass');
 				var that = this;
 				//var canvas=this.document.getElementById("canvas");
@@ -32,36 +33,54 @@
 				ctx.draw();
 				console.log("1");
 
-				setTimeout(function() {
-					console.log('666')
-					ctx.clearRect(0,0,1060,700)
-					 that.x = that.x+1;
-					 that.y= that.y+1;
-					 that.drawCanvas(ctx,that.x, that.y);
-				}, 2000)
+				// setTimeout(function() {
+				// 	console.log('666')
+				// 	ctx.clearRect(0,0,1060,700)
+				// 	 that.x = that.x+1;
+				// 	 that.y= that.y+1;
+				// 	 that.drawCanvas(ctx,that.x, that.y);
+				// }, 10)
 				// context.clearRect(0, 0, 200, 200)
 				// context.draw(false, setTimeout((res) => {
 				// 	console.log('cbk');
 				// 	console.log(res);
 				// }, 1000))
 
+				
 			},
-
-			// drawPic(ctx, personPic, posX, posY, name) { //绘制人物图标
-			// 	var _this = this;
-			// 	ctx.drawImage(personPic, parseFloat(posX) + _this.x, parseFloat(posY) + _this.y, 20, 27);
-			// 	ctx.font = "11px Arial";
-			// 	ctx.fillStyle = "black"
-			// 	ctx.fillText(name, parseFloat(posX) + _this.x + 25, parseFloat(posY) + _this.y + 15);
-			// },
-
-			canvasIdErrorCallback: function(e) {
-				console.log('cer');
-				console.error(e)
-				debugger;
+			
+			touchStart(event) {
+				var ctx = uni.createCanvasContext('canvass');
+				var that = this;
+				var imgx = that.x;
+				var imgy = that.y;
+				console.log(event);
+				var pos = {
+					x: event.touches[0].x,
+					y: event.touches[0].y
+				}; //坐标转换，将窗口坐标转换成canvas的坐标
+			// 	ctx.onmousemove = function(evt) { //移动
+			// 		ctx.style.cursor = 'move';
+			
+			// 		var x = (evt.clientX - pos.x) * 2 + imgx;
+			// 		var y = (evt.clientY - pos.y) * 2 + imgy;
+			// 		that.x = x;
+			// 		that.y = y;
+			// 		that.drawImage(that.imgObject); //重新绘制图片
+			// 	};
+			// 	ctx.onmouseup = function() {
+			// 		ctx.onmousemove = null;
+			// 		ctx.onmouseup = null;
+			// 		ctx.style.cursor = 'default';
+			// 	};
+			},
+					canvasIdErrorCallback: function(e) {
+						console.log('cer');
+						console.error(e)
+						debugger;
+					}
 			}
 		}
-	}
 </script>
 <style>
 	#canvass {
